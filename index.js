a8895737dc36a97f5f11936173526c2a0d902675
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-14 14:29:06 -0400

    Modal working - Need to implement user input

--- index.js
+++ index.js
@@ -1,53 +1,128 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 let size = 16;
-let color = 'black';
+let color = "black";
 
-function setupScreen() {
-    createHeading();
-    createContainer();
-    createGrid(16, 16);
-    addListeners();
+function setupGame() {
+  createHeading();
+  createPopup();
+  createContainer();
+  createGrid(size);
+  addListeners();
 }
 
 function createHeading() {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Etch-A-Sketch';
-    body.appendChild(h1);
+  const h1 = document.createElement("h1");
+  h1.textContent = "Etch-A-Sketch";
+  h1.style.marginBottom = 0;
+  body.appendChild(h1);
+}
+
+function createPopup() {
+  createModalButton();
+  createSection();
+}
+
+function createModalButton() {
+  const modalButton = document.createElement("button");
+  modalButton.className = "modal-btn";
+  modalButton.textContent = "New Grid";
+  modalButton.style.margin = "5px";
+  body.appendChild(modalButton);
+}
+
+function createSection() {
+  // Whole section
+  const section = document.createElement("section");
+  section.className = "modal hidden";
+
+  // Div with close button
+  const flexDiv = document.createElement("div");
+  flexDiv.className = "flex";
+
+  const closeButton = document.createElement("button");
+  closeButton.className = "btn-close";
+  closeButton.textContent = "⨉";
+
+  flexDiv.appendChild(closeButton);
+  section.appendChild(flexDiv);
+
+  // User input
+  const inputArea = document.createElement("input");
+  inputArea.type = "text";
+  inputArea.id = "size-input";
+  inputArea.placeholder = "What size?";
+
+  const submitInput = document.createElement("button");
+  submitInput.className = "btn";
+  submitInput.textContent = "Submit";
+
+  section.appendChild(inputArea);
+  section.appendChild(submitInput);
+
+  body.appendChild(section);
+
+  // Hidden div for modal
+  const overlayDiv = document.createElement("div");
+  overlayDiv.className = "overlay hidden";
+  body.appendChild(overlayDiv);
+}
+
+function openModal() {
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
 
 function createContainer() {
-    const container = document.createElement('div');
-    container.id = 'container';
-    body.appendChild(container);
+  const container = document.createElement("div");
+  container.id = "container";
+  body.appendChild(container);
 }
 
 function createGrid(size) {
-    const container = document.getElementById('container');
-    container.style.display = 'grid';
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    container.style.maxHeight = '100vh';
+  const container = document.getElementById("container");
+  container.style.display = "grid";
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.maxHeight = "100vh";
 
-    for (let i = 0; i < (size * size); i++) {
-        const square = document.createElement('div');
-        square.className = 'square';
-        square.style.border = '1px solid black';
-        square.style.height = '1.5rem';
-        square.style.width = '1.5rem';
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    square.style.border = "1px solid black";
+    square.style.height = "1.5rem";
+    square.style.width = "1.5rem";
 
-        container.appendChild(square);
-    };
+    container.appendChild(square);
+  }
 
-    body.appendChild(container);
+  body.appendChild(container);
 }
 
 function addListeners() {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-        square.addEventListener('mouseenter', function(e) {
-            e.target.style.backgroundColor = color;
-        });
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", function (e) {
+      e.target.style.backgroundColor = color;
     });
+  });
+
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
+  const openModalBtn = document.querySelector(".modal-btn");
+  const closeModalBtn = document.querySelector(".btn-close");
+
+  openModalBtn.addEventListener("click", openModal);
+  closeModalBtn.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
 }
 
-setupScreen();
+setupGame();
